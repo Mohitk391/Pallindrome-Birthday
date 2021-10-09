7f22679a5c54bff308e8c648c1fcfb92ecbdae82
Mohitk391 <mohitkpatel391@gmail.com>
2021-10-09 23:06:05 +0530

    Updated backend

--- app.js
+++ app.js
@@ -5,18 +5,24 @@ var output = document.querySelector("#output");
 function checkPallindrome(){
     console.log("date: " + birthdate.value);
     console.log("typeof date: " + typeof birthdate.value);
-    date = birthdate.value;
-    var first = date.indexOf("-");
-    var second = date.lastIndexOf("-");
-    var strDate = date.substring(second+1,10) + date.substring(first+1,second) + date.substring(0,first);
-    console.log("curDate: " + strDate);
-    var revDate = strDate.split('').reverse().join('');
-    console.log("reverse Date: "+ revDate);
-    if(strDate == revDate){
-        output.innerText = "Your birthday is a Pallindrome!!!";
+    var date = birthdate.value;
+    if(date == ""){
+        console.log("blank");
     }
     else {
-        output.innerText = "Alas! your birthdate is not Pallindrome";
+        var first = date.indexOf("-");
+        var second = date.lastIndexOf("-");
+        var strDate = date.substring(second+1,10) + date.substring(first+1,second) + date.substring(0,first);
+        console.log("curDate: " + strDate);
+        var revDate = strDate.split('').reverse().join('');
+        console.log("reverse Date: "+ revDate);
+        
+        if(strDate == revDate){
+            output.innerText = "Your birthday is a Pallindrome!!!";
+        }
+        else {
+            output.innerText = "Alas! your birthdate is not Pallindrome";
+        }
     }
 }
 
